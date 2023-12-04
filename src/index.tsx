6e9b1ac758c5cb18299bf7b6cd354b424fa41324
Jordan Pittman <jordan@cryptica.me>
2023-12-04 18:19:46 -0500

    Fix code style

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import { ActionPanel, Action, List, Icon } from "@raycast/api";
 import { useCachedPromise, useFrecencySorting } from "@raycast/utils";
 import { useState } from "react";
-import { selectedFinderItems } from "swift:../swift/open-with"
+import { selectedFinderItems } from "swift:../swift/open-with";
 
 export default function Command() {
-  const { data, isLoading } = useFinderSelection()
+  const { data, isLoading } = useFinderSelection();
 
-  const { files, apps, target} = {
+  const { files, apps, target } = {
     files: data?.files ?? [],
     apps: data?.apps ?? [],
     target: data?.files[0] ?? undefined,
-  }
+  };
 
   const { data: sortedApps, visitItem } = useFrecencySorting(apps, {
     key: (item) => item.path,
   });
 
-  const [ searchText, setSearchText ] = useState("")
+  const [searchText, setSearchText] = useState("");
 
   return (
     <List
@@ -25,49 +25,43 @@ export default function Command() {
       searchText={searchText}
       onSearchTextChange={setSearchText}
       searchBarPlaceholder={
-        isLoading
-          ? "Loading..."
-          : target
-            ? `Open "${target.name}" with...`
-            : `Select a file in the Finder to open...`
-        }
+        isLoading ? "Loading..." : target ? `Open "${target.name}" with...` : `Select a file in the Finder to open...`
+      }
     >
-      {isLoading ? (null) : (
-        files.length === 0 ? (
-          <List.EmptyView
-            icon={{ source: Icon.FountainTip }}
-            title="No files selected"
-            description="Select a file in the finder first"
-          />
-        ) : apps.length === 0 ? (
-          <List.EmptyView
-            icon={{ source: Icon.FountainTip }}
-            title="No apps found"
-            description={`No apps found that can open ${target!.name}`}
-          />
-        ) : (
-          <>
-            <List.Section title="Applications" subtitle={`${apps.length}`}>
-              {sortedApps.map((app) => (
-                <AppItem
-                  key={app.path}
-                  target={target!}
-                  app={app}
-                  onOpen={() => {
-                    setSearchText("")
-                    return visitItem(app)
-                  }}
-                />
-              ))}
-            </List.Section>
-          </>
-        )
+      {isLoading ? null : files.length === 0 ? (
+        <List.EmptyView
+          icon={{ source: Icon.FountainTip }}
+          title="No files selected"
+          description="Select a file in the finder first"
+        />
+      ) : apps.length === 0 ? (
+        <List.EmptyView
+          icon={{ source: Icon.FountainTip }}
+          title="No apps found"
+          description={`No apps found that can open ${target!.name}`}
+        />
+      ) : (
+        <>
+          <List.Section title="Applications" subtitle={`${apps.length}`}>
+            {sortedApps.map((app) => (
+              <AppItem
+                key={app.path}
+                target={target!}
+                app={app}
+                onOpen={() => {
+                  setSearchText("");
+                  return visitItem(app);
+                }}
+              />
+            ))}
+          </List.Section>
+        </>
       )}
     </List>
   );
 }
 
-function AppItem({ target, app, onOpen }: { app: File, target: File, onOpen?: () => void }) {
+function AppItem({ target, app, onOpen }: { app: File; target: File; onOpen?: () => void }) {
   return (
     <List.Item
       icon={{ fileIcon: app.path }}
@@ -92,27 +86,21 @@ function AppItem({ target, app, onOpen }: { app: File, target: File, onOpen?: ()
 }
 
 function useFinderSelection() {
-  const { isLoading, data, revalidate } = useCachedPromise(async () => {
-    let start = performance.now()
-    let result = await selectedFinderItems<Selection>()
-    let end = performance.now()
-    console.log(`selectedFinderItems took ${end - start}ms`)
-    return result
-  })
+  const { isLoading, data, revalidate } = useCachedPromise(() => selectedFinderItems<Selection>());
 
   return {
     isLoading,
     data,
     revalidate,
-  }
+  };
 }
 
 interface File {
-  name: string
-  path: string
+  name: string;
+  path: string;
 }
 
 interface Selection {
-  files: File[]
-  apps: File[]
+  files: File[];
+  apps: File[];
 }
